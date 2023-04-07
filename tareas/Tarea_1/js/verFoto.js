4025941ff51334a093f95e54af700dde55e107c6
MagicDog12 <creature.secret@gmail.com>
2023-04-06 22:51:44 -0400

    se agrega la informacion de donacion y se optimiza verFoto.js

--- tareas/Tarea_1/js/verFoto.js
+++ tareas/Tarea_1/js/verFoto.js
@@ -1,13 +1,11 @@
-let cond = false;
+
 const verFoto = () => {
     const foto = document.getElementById("foto");
-    if(!cond) {
+    if(foto.style.width == "640px") {
         foto.style.width = "1280px";
         foto.style.height = "1024px";
-        cond = true;
     } else {
         foto.style.width = "640px";
         foto.style.height = "480px";
-        cond = false;
     }
 };
